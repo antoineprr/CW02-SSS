ca1497400cf0b349895f9ccc5ab1aacdeb742a0e
antoineprr <antoine.poirier@etu.imt-nord-europe.fr>
2025-11-21 12:39:15 +0000

    feat: creation migrations, controllers and models files + structure db with migrations

---
+++ database/migrations/2025_11_21_121833_create_players_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_id');
+            $table->foreignId('position_id');
+            $table->foreignId('country_id');
+            $table->string('name');
+            $table->string('firstname');
+            $table->integer('age');
+            $table->string('description');
+            // TODO : add picture
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('players');
+    }
+};
