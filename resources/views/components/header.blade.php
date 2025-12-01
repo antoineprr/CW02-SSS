b6347cec1b367fd6fa22f37a4e5926662d731fba
antoineprr <antoine.poirier@etu.imt-nord-europe.fr>
2025-12-01 18:05:17 +0000

    feat: change dropdown header to icon

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -7,19 +7,24 @@
 
         <div class="absolute top-1/2 transform -translate-y-1/2 right-0 mr-2 text-right">
             @auth
-                <div class="flex items-center ms-4">
+                <div class="flex items-center mr-2">
                     <x-dropdown>
                         <x-slot name="trigger">
-                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-slate-800 bg-[#E0B7C3] hover:text-slate-950 focus:outline-none transition ease-in-out duration-150">
-                                <div>{{ Auth::user()->name }} {{ Auth::user()->firstname }}</div>
-
-                                <div class="ms-1">
-                                    <svg class="fill-current h-4 w-4 text-slate-800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                    </svg>
-                                </div>
+                            <button class="transition ease-in-out duration-150 h-11 w-11 rounded-full border-2 bg-[#E0B7C3] flex items-center justify-center overflow-hidden">
+                                @if (Auth::user()->picture && Storage::disk('public')->exists(Auth::user()->picture))
+                                    <img 
+                                        src="{{ asset('storage/' . Auth::user()->picture) }}" 
+                                        alt="Current photo" 
+                                        class="h-full w-full object-cover"
+                                    >
+                                @else
+                                    <span class="text-slate-800 font-medium">
+                                        {{ strtoupper(substr(Auth::user()->firstname, 0, 1) . substr(Auth::user()->name, 0, 1)) }}
+                                    </span>
+                                @endif
                             </button>
                         </x-slot>
+
 
                         <x-slot name="content">
                             <x-dropdown-link :href="route('profile.edit')">
@@ -36,19 +41,10 @@
                             </form>
                         </x-slot>
                     </x-dropdown>
-                    <div class="ml-2">
-                        @if (Auth::user()->picture && Storage::disk('public')->exists(Auth::user()->picture))
-                            <img 
-                                src="{{ asset('storage/' . Auth::user()->picture) }}" 
-                                alt="Current photo" 
-                                class="h-10 w-10 rounded-full object-cover"
-                            >
-                        @endif
-                    </div>
                 </div>
 
-                
             @else
+
                 <a href="{{ route('login') }}"
                     class="font-semibold text-[#E0B7C3] hover:text-[#F2CEDA] transition">
                     Log in
@@ -58,6 +54,7 @@
                     class="ml-4 font-semibold text-[#E0B7C3] hover:text-[#F2CEDA] transition">
                     Register
                 </a>
+
             @endauth
         </div>
 
